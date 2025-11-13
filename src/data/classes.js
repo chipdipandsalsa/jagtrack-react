6a90b4315ec9cfbde088d479b0b4227fde186286
Ashlyn Wolfe <WolfeA@JEFFERSON.LOCAL>
2025-11-13 13:38:29 -0700

    Personalization

--- src/data/classes.js
+++ src/data/classes.js
@@ -2,37 +2,37 @@ export const classes = [
  {
    id: 1,
    block: 'A Block',
-   name: 'English Literature',
+   name: 'Modern US History',
    time: '8:25 AM - 9:55 AM',
-   description: 'Explore classic and contemporary literature while developing critical reading and writing skills.',
-   canvasUrl: '#',
+   description: 'Explore the history of the US, detailing topics from the beginning of the US all the way to the Cold War.',
+   canvasUrl: 'https://jajags.instructure.com/courses/3326',
    color: '#e03131'
  },
  {
    id: 2,
    block: 'B Block',
-   name: 'JavaScript Programming',
+   name: 'Spanish III',
    time: '10:00 AM - 11:30 AM',
-   description: 'Learn to build modern, responsive websites and interactive web applications using React.',
-   canvasUrl: 'https://jajags.instructure.com/courses/3439',
+   description: 'Touch up on your Spanish skills and expand your knowledge on the conjugations or verbs!',
+   canvasUrl: 'https://jajags.instructure.com/courses/3347',
    color: '#1a5490'
  },
  {
    id: 3,
    block: 'C Block',
-   name: 'Algebra II',
+   name: 'JavaScript Programming',
    time: '12:15 AM - 1:45 PM',
-   description: 'Master advanced algebraic concepts including functions, polynomials, exponentials, and logarithms.',
-   canvasUrl: '#',
+   description: 'Learn to build modern, responsive websites and interactive web applications using React.',
+   canvasUrl: 'https://jajags.instructure.com/courses/3439',
    color: '#2f9e44'
  },
  {
    id: 4,
    block: 'D Block',
-   name: 'Biology',
+   name: 'Chemistry',
    time: '1:50 PM - 3:20 PM',
-   description: 'Investigate living systems from cells to ecosystems through hands-on lab work.',
-   canvasUrl: '#',
+   description: 'Investigate the elements that make up life and how they combine, with hands-on labs and in-person experiences!',
+   canvasUrl: 'https://jajags.instructure.com/courses/3340',
    color: '#f08c00'
  }
 ]
